a0078822356b6a3fb59b98a93adaa6c8b61d8019
vedantpuri <vedantpuri13@gmail.com>
2020-06-07 01:25:47 -0400

    Switch from OOP to script + Add dialogue json

--- utils/dialogue_parser.py
+++ utils/dialogue_parser.py
@@ -1,90 +1,144 @@
+import re
+import sys
+import json
 import pickle
 import random
 import logging
 from nltk.corpus import stopwords
 
-class DialogueParser:
-    """
-    A class to be used to read and wrangle the dialogue data
-    """
-    def __init__(self, fname, delimeter="__eou__", lower=True, stop_rem=True):
-        """
-        :param fname:       File name to be read in
-        :param delimeter:   Token that delimets dialogues
-        :param lower:       Boolean specifying conversion to lowercase
-        :param stop_rem:    Boolean specifying stopword removal
-        """
-        self.fname = fname
-        self.delimeter = delimeter
-        self.lower = lower
-        self.stop_rem = stop_rem
-        logging.basicConfig(format='%(asctime)s %(message)s')
-        self.logger = logging.getLogger("dialogue_parser")
-        self.logger.setLevel(logging.DEBUG)
-        self.data = []
+"""
+Running command:
+python dialogue_parser.py **path to dialogues_text.txt** ** path to dialogues_emotion.txt** ../data/dialogue_ems.json
+"""
 
-    def read_file(self):
-        """Read the file provided"""
-        self.logger.debug("Reading file")
-        ctr = 0
-        with open(self.fname) as f:
-            content = f.read()
-        dialogue = []
-        for word in content.split():
-            if word == self.delimeter:
-                self.data += [self.pre_process(" ".join(dialogue))]
+
+def read_dialogues(fname, delimeter):
+    """
+    Read the dialogue file provided
+    :param fname:       The file name to be read
+    :param delimeter:   The token demarcating dialogues
+    :return:            pre-processed dialogues
+    """
+    with open(fname) as f:
+        content = f.readlines()
+    logger.debug(f"Reading dialogue file: {fname}")
+    ctr = 0
+    dialogue = []
+    data = []
+    for line in content:
+        for word in line.split():
+            if word == delimeter:
+                data += [" ".join(dialogue)]
                 dialogue = []
                 ctr += 1
-                print(ctr)
             else:
                 dialogue += [word]
-        self.logger.debug("Reading complete")
+    logger.debug(f"Successfully read {ctr} utterances.")
+    return data
 
-    def pre_process(self, dialogue):
-        """
-        Preprocess based on settings
-        :param dialogue:    The dialogue to be preprocessed
-        :return:            Preprocessed dialogue
-        """
-        ret = []
-        for word in dialogue.split():
-            if self.lower or self.stop_rem:
-                if self.lower:
-                    word = word.lower()
-                if self.stop_rem and word not in stopwords.words('english'):
-                    ret += [word]
-            else:
+
+def pre_process(dialogue, lower=False, stop_rem=False):
+    """
+    Preprocess based on settings
+    :param dialogue:    The dialogue to be preprocessed
+    :return:            Preprocessed dialogue
+    """
+    ret = []
+    for word in dialogue.split():
+        if lower or stop_rem:
+            if lower:
+                word = word.lower()
+            if stop_rem and word not in stopwords.words("english"):
                 ret += [word]
-        return " ".join(ret)
+        else:
+            ret += [word]
+    return " ".join(ret)
 
 
-    def dump_data(self, pickle_file):
-        """
-        Dump data to a pickle file
-        :param pickle_file:     Dump destination
-        """
-        if not self.data:
-            self.logger.error("Nothing to dump, read file first")
-            return
-        pickle.dump(self.data, open(pickle_file, "wb"))
+def read_emotions(fname):
+    """
+    Read the emotion file provided
+    :param fname:       The file name to be read
+    :return:            list of emotions
+    """
+    with open(fname) as f:
+        content = f.readlines()
+    logger.debug(f"Reading emotions file: {fname}")
+    ctr = 0
+    emotions = []
+    for line in content:
+        ems = line.strip().split()
+        for emotion in ems:
+            emotions += [emotion]
+            ctr += 1
+    logger.debug(f"Successfully read {ctr} emotions.")
+    return emotions
 
-    def load_data(self, pickle_file):
-        """
-        Load data from pickle file
-        :param pickle_file:     Load destination
-        :return:                Loaded data
-        """
-        return pickle.load(open(pickle_file, "rb"))
 
-    @staticmethod
-    def print_list(content):
-        for element in content:
-            print(element)
+# ----- I/O utils
+def print_list(content):
+    """Pretty print a list"""
+    for element in content:
+        print(element)
 
-    def display_data(self, limit=50):
-        """
-        Show any 50 instances
-        :param limit:   Limit to how many instances to display
-        """
-        random.shuffle(self.data)
-        DialogueParser.print_list(self.data[:limit])
+
+def display_data(data, limit=50):
+    """
+    Show any 50 instances
+    :param limit:   Limit to how many instances to display
+    """
+    random.shuffle(data)
+    print_list(data[:limit])
+
+
+def unite_data(data, emotions, filter_noemo=True):
+    """
+    Map emotions to utterances
+    :param data:            List of utterances
+    :param emotions:        List of emotions
+    :param filter_noemo:    Filter out no emotions
+    """
+    logger.debug(f"Mapping emotions to utterances, Filtering: {filter_noemo}")
+    united_data = []
+    for i in range(len(data)):
+        utt = data[i]
+        # Remove weird right quotation marks
+        utt = re.sub("(\u2018|\u2019)", "'", utt)
+        if filter_noemo:
+            if emotions[i] != NO_EMOTION:
+                united_data += [(utt, emotions[i])]
+        else:
+            united_data += [(utt, emotions[i])]
+    logger.debug(f"Mapping complete. Total instances: {len(united_data)}")
+    return united_data
+
+
+def dump2json(data, json_file):
+    logger.debug(f"Dumpting to json file: {json_file}")
+    dump_data = []
+    for element in data:
+        dump_data += [{"utterance": element[0], "emotion": element[1]}]
+    with open(json_file, "w") as fout:
+        json.dump(dump_data, fout, indent=2)
+    logger.debug(f"Dumping complete")
+
+
+# ----- Main execution
+if __name__ == "__main__":
+    # Constants
+    NO_EMOTION = "0"
+
+    # Logging setup
+    logging.basicConfig(format="%(asctime)s %(message)s")
+    logger = logging.getLogger("dialogue_parser")
+    logger.setLevel(logging.DEBUG)
+
+    dialogue_fname, emotion_fname, json_out = sys.argv[1], sys.argv[2], sys.argv[3]
+    delimeter = "__eou__"
+    data = read_dialogues(dialogue_fname, delimeter)
+    emotions = read_emotions(emotion_fname)
+    # Sanity check
+    assert len(data) == len(emotions)
+
+    united_data = unite_data(data, emotions)
+    dump2json(united_data, json_out)
